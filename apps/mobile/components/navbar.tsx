872410f04b179f43af0fd7b6c7a8978e2bc12955
Hampus Söderlund <hampus_2001@hotmail.com>
2025-11-10 11:46:20 +0100

    fixade en component för naven i mobile och implementerade den i index och slug. Skapade en Cart page.

---
+++ apps/mobile/components/navbar.tsx
@@ -0,0 +1,43 @@
+import { cart, filterBar, logo, profile } from "@/assets/images";
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import { TouchableOpacity } from "react-native";
+import { router } from "expo-router";
+
+export function Navbar() {
+  return (
+    <View style={styles.navbar}>
+      <View style={styles.left}>
+        <TouchableOpacity onPress={() => router.back()}>
+          <Image source={filterBar} style={{ width: 26, height: 16 }} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.center}>
+        <Image source={logo} style={{ width: 29, height: 29 }} />
+      </View>
+      <View style={styles.right}>
+        <TouchableOpacity>
+          <Image source={cart} style={{ width: 41, height: 41 }} />
+        </TouchableOpacity>
+        <Image source={profile} style={{ width: 41, height: 41 }} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  navbar: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingTop: 24,
+    paddingHorizontal: 18,
+  },
+  left: { flex: 1 },
+  center: { flex: 1, alignItems: "center" },
+  right: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    gap: 6,
+  },
+});
